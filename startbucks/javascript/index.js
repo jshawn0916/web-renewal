ad54aeef5cc1bad69011bbf3b133af9645a82f55
jshawn0916 <seyul_@naver.com>
2021-10-29 17:29:04 +0900

    2021-10-28까지 수정

--- startbucks/javascript/index.js
+++ startbucks/javascript/index.js
@@ -1,44 +1,55 @@
+//menu-btn 클릭시 스크롤 이동 효과
 
-document.querySelectorAll('.city-btn').forEach(function(anchor){
-    anchor.onclick = function(e){
-        document.querySelectorAll('.city-btn').forEach(function(ele){
-            ele.classList.remove('on');
-        })
-        anchor.classList.add('on');
-        e.preventDefault()
-        const name = anchor.getAttribute('name')
-        const target = document.getElementById(name)
-        const to = target.offsetTop
-        scrollTo(document.documentElement, to, 2000)
-    }
-})
-
-const scrollTo = function(element, to, duration){
-    let start = element.offsetTop
-    // console.log(element)
-    let change = to - start
-    let currentTime = 0
-    let increment = 20;
-
-    const animateScroll = function(){
-        currentTime += increment;
-        const val = easeInOutQuad(currentTime, start, change, duration);
-        element.scrollTop = val;
-        if(currentTime < duration){
-            setTimeout(animateScroll, increment);
+document.querySelectorAll('a[href^="#"]').forEach(function(elem){
+    elem.addEventListener('click', function(e){
+        e.preventDefault();
+        for(let i = 0; i < document.getElementsByClassName('menu-btn').length; i++){
+            document.getElementsByClassName('menu-btn')[i].classList.remove('on')
         }
+        elem.classList.add('on');
+        document.querySelector(elem.getAttribute('href')).scrollIntoView({     
+            behavior: 'smooth',
+            offsetTop: 20
+        });
+    });
+});
+
+//패럴렉스 스크롤
+let basic_scroll = document.querySelector('html').scrollTop //현재위치값
+let basic_height = window.innerHeight //브라우저 높이
+let basic_section_num = basic_scroll / basic_height;
+document.getElementById('menu'+(basic_section_num + 1)).click();//페이지에 맞는 menu버튼 클릭
+
+let event1 = true;
+let lastPage = document.getElementsByClassName("content-box").length - 1;
+let page = basic_section_num;
+window.addEventListener("wheel", function(e){
+    if(!event1) { return false; }
+    event1 = false;
+    if(e.deltaY > 0){
+        if(page == lastPage) {
+            event1 = true 
+            return
+        };
+        page++;
+        
+    }else if(e.deltaY < 0){
+        if(page == 0) {
+            event1 = true 
+            return;
+        }
+        page--;
     }
+    for(let i = 0; i< document.getElementsByClassName("menu-btn").length; i++){
+        document.getElementsByClassName("menu-btn")[i].classList.remove("on") 
+    }
+    document.getElementsByClassName("menu-btn")[page].classList.add("on");
+    let box1_top = document.getElementsByClassName("content-box")[page].offsetTop;
+    window.scrollTo({top: box1_top, behavior: "smooth"})
+    setTimeout(function(){
+       event1 = true 
+    },400)
+    // var posTop = (page-1) * $(window).height();
 
-    animateScroll()
-}
-//t = current time
-//b = start value
-//c = change in value
-//d = duration
-const easeInOutQuad = (t, b, c, d) => {
-    t /= d / 2
-    if (t < 1) return c / 2 * t * t + b
-    t--
-    return -c / 2 * (t * (t - 2) - 1) + b
-}
-
+    // htmlBody.animate({scrollTop : posTop});
+});
