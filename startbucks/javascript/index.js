0a6a44f79a55e817bd75e805cee3c087e11b37f3
jshawn0916 <seyul_@naver.com>
2021-11-11 14:48:18 +0900

    2021-11-10

--- startbucks/javascript/index.js
+++ startbucks/javascript/index.js
@@ -15,46 +15,46 @@ document.querySelectorAll('a[href^="#"]').forEach(function(elem){
     });
 });
 
-//패럴렉스 스크롤
-let basic_scroll = document.querySelector('html').scrollTop //현재위치값
-let basic_height = window.innerHeight //브라우저 높이
-let basic_section_num = basic_scroll / basic_height;
-document.getElementById('menu'+(basic_section_num + 1)).click();//페이지에 맞는 menu버튼 클릭
+//새로고침시, 스크롤 맨위로
+let scroll_stay = false;
+let _page = document.querySelectorAll('.content-box');
+let _btn = document.querySelectorAll('.menu-btn');
+let _count = 0;
+let topIndex = [];
 
-let event1 = true;
-let lastPage = document.getElementsByClassName("content-box").length - 1;
-let page = basic_section_num;
-window.addEventListener("wheel", function(e){
-    if(!event1) { return false; }
-    event1 = false;
-    if(e.deltaY > 0){
-        if(page == lastPage) {
-            event1 = true 
-            return
-        };
-        page++;
-        
-    }else if(e.deltaY < 0){
-        if(page == 0) {
-            event1 = true 
+_page.forEach(function(ele){
+    topIndex.push(ele.offsetTop);
+});
+    window.addEventListener('mousewheel', function(e) {
+        if(scroll_stay){
+            console.log('stay');
             return;
         }
-        page--;
-    }
-    for(let i = 0; i < document.getElementsByClassName("menu-btn").length; i++){
-        document.getElementsByClassName("menu-btn")[i].classList.remove("on") 
-    }
-    document.getElementsByClassName("menu-btn")[page].classList.add("on");
-    let box1_top = document.getElementsByClassName("content-box")[page].offsetTop;
-    window.scrollTo({top: box1_top, behavior: "smooth"})
-    setTimeout(function(){
-       event1 = true 
-    },400)
-    // var posTop = (page-1) * $(window).height();
-
-    // htmlBody.animate({scrollTop : posTop});
-});
-//새로고침시, 스크롤 맨위로
-window.onload = function(){
-//   document.getElementById('menu1').click();
-}
+        scroll_stay = true;
+        document.getElementById('menu'+_count).classList.remove('on');
+        console.log(_count)
+        if(e.wheelDelta > 0 && _count >= 0){
+            _count--;
+            window.scrollTo({top : topIndex[_count], behavior:'smooth'})
+            document.getElementById('menu'+_count).classList.add('on');
+            // console.log('up'+_count)
+            //지금위치에서 한칸 위로
+        }else if(_count < 5 && _count >= 0){
+            _count++;
+            window.scrollTo({top : topIndex[_count], behavior:'smooth'})
+            document.getElementById('menu'+_count).classList.add('on');
+            // console.log('down'+_count);
+            //지금위치에서 한칸 아래로
+        }else{
+            scroll_stay = false;
+            return 
+        }
+    
+        setTimeout(function(){
+            scroll_stay = false;
+            console.log(2222222)
+        },300);
+        
+        console.log(111111)
+        // console.log(e.wheelDelta);
+    })
