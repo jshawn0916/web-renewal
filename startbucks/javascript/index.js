29d7894fe387fad0401405d002dc853f36200f2d
jshawn0916 <seyul_@naver.com>
2021-11-11 17:02:09 +0900

    2021-11-11

--- startbucks/javascript/index.js
+++ startbucks/javascript/index.js
@@ -27,34 +27,33 @@ _page.forEach(function(ele){
 });
     window.addEventListener('mousewheel', function(e) {
         if(scroll_stay){
-            console.log('stay');
+            console.log('stay' + _count);
             return;
         }
         scroll_stay = true;
+        
         document.getElementById('menu'+_count).classList.remove('on');
-        console.log(_count)
-        if(e.wheelDelta > 0 && _count >= 0){
+        if(e.wheelDelta > 0 && _count > 0){
             _count--;
             window.scrollTo({top : topIndex[_count], behavior:'smooth'})
             document.getElementById('menu'+_count).classList.add('on');
-            // console.log('up'+_count)
+            console.log('up'+_count)
             //지금위치에서 한칸 위로
-        }else if(_count < 5 && _count >= 0){
+        }else if(_count >= 0 && _count < 4){
             _count++;
             window.scrollTo({top : topIndex[_count], behavior:'smooth'})
             document.getElementById('menu'+_count).classList.add('on');
-            // console.log('down'+_count);
+            console.log('down'+_count);
             //지금위치에서 한칸 아래로
         }else{
-            scroll_stay = false;
-            return 
+            console.log('0보다 작거나 4보다 크다')
+            return;
         }
-    
-        setTimeout(function(){
-            scroll_stay = false;
-            console.log(2222222)
-        },300);
+       
         
-        console.log(111111)
-        // console.log(e.wheelDelta);
+        // console.log(scroll_stay)
+        scroll_stay = false;
+        // setTimeout(function(){
+        //     console.log(2222222)
+        // },300);
     })
