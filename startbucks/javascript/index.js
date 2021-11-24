8ecae6be2558a112b611f3fb26c4d0e329a82740
jshawn0916 <seyul_@naver.com>
2021-11-24 16:05:13 +0900

    .

--- startbucks/javascript/index.js
+++ startbucks/javascript/index.js
@@ -19,8 +19,9 @@ document.querySelectorAll('a[href^="#"]').forEach(function(elem){
 let scroll_stay = false;
 let _page = document.querySelectorAll('.content-box');
 let _btn = document.querySelectorAll('.menu-btn');
-let _count = 0;
+let _count = Math.round(document.documentElement.scrollTop / window.innerHeight);
 let topIndex = [];
+document.getElementById('menu'+_count).classList.add('on');
 
 _page.forEach(function(ele){
     topIndex.push(ele.offsetTop);
@@ -34,26 +35,37 @@ _page.forEach(function(ele){
         
         document.getElementById('menu'+_count).classList.remove('on');
         if(e.wheelDelta > 0 && _count > 0){
+            console.log(11111)
             _count--;
             window.scrollTo({top : topIndex[_count], behavior:'smooth'})
             document.getElementById('menu'+_count).classList.add('on');
             console.log('up'+_count)
             //지금위치에서 한칸 위로
-        }else if(_count >= 0 && _count < 4){
+        }else if(e.wheelDelta < 0 && _count < 4){
             _count++;
             window.scrollTo({top : topIndex[_count], behavior:'smooth'})
             document.getElementById('menu'+_count).classList.add('on');
             console.log('down'+_count);
             //지금위치에서 한칸 아래로
         }else{
-            console.log('0보다 작거나 4보다 크다')
+            console.log('0보다 작거나 4보다 크다');
+            document.getElementById('menu'+_count).classList.add('on');
+            scroll_stay = false;
             return;
         }
        
         
         // console.log(scroll_stay)
-        scroll_stay = false;
-        // setTimeout(function(){
-        //     console.log(2222222)
-        // },300);
-    })
+        setTimeout(function(){
+            scroll_stay = false;
+            console.log(2222222)
+        },500);
+    });
+
+
+// window.onload = function(){
+//     console.log(document.documentElement.scrollTop, window.innerHeight)
+//     setTimeout(function(){
+//         document.getElementById('menu0').click()
+//     },300)
+// };
